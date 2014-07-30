bfe08dd9b81a11502adf6ed161930504458b1780
dormon <dormon@papovetrak.(none)>
2014-07-31 00:26:44 +0200

    Added ShadersAndPrograms folder that contains ShaderObjectManager and ShaderObjectsCommands

---
+++ include/geGL/ShadersAndPrograms/ShaderObjectManager.h
@@ -0,0 +1,31 @@
+#ifndef _SHADEROBJECTMANAGER_H_
+#define _SHADEROBJECTMANAGER_H_
+
+#include<iostream>
+
+#include<geGL/Standard/OpenGL.h>
+#include<geGL/ShadersAndPrograms/ShaderObjectsCommands.h>
+
+namespace ge
+{
+  namespace ogl
+  {
+    class GE_EXPORT CreateShaderObject: public Command
+    {
+      private:
+        CreateShader  * _createShader;
+        ShaderSource  * _shaderSource;
+        CompileShader * _compileShader;
+      public:
+        CreateShaderObject(
+            GLuint*shader,
+            GLenum type,
+            std::string text);
+        ~CreateShaderObject();
+        void apply();
+    };
+  }//ogl
+}//ge
+
+
+#endif//_SHADEROBJECTMANAGER_H_
---
+++ src/geGL/ShadersAndPrograms/ShaderObjectManager.cpp
@@ -0,0 +1,30 @@
+#include<geGL/ShadersAndPrograms/ShaderObjectManager.h>
+
+namespace ge
+{
+  namespace ogl
+  {
+    CreateShaderObject::CreateShaderObject(
+        GLuint*shader,
+        GLenum type,
+        std::string text){
+      GLchar**string=new GLchar*[1];
+      string[1]=new GLchar[text.size()+1];
+      for(unsigned i=0;i<text.size();++i)string[1][i]=text[i];
+      string[1][text.size()]='\0';
+      this->_createShader  = new CreateShader (shader,type);
+      this->_shaderSource  = new ShaderSource (shader,1,string,NULL);
+      this->_compileShader = new CompileShader(shader);
+    }
+    CreateShaderObject::~CreateShaderObject(){
+      delete this->_createShader;
+      delete this->_shaderSource;
+      delete this->_compileShader;
+    }
+    void CreateShaderObject::apply(){
+      this->_createShader ->apply();
+      this->_shaderSource ->apply();
+      this->_compileShader->apply();
+    }
+  }//ogl
+}//ge
