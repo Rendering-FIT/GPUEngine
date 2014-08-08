72c59c116a0ea9c7c9479670d2603b1afe138b7e
foreigner <starka@fit.vutbr.cz>
2014-08-08 14:17:13 +0200

    Buffer Object  * Some tweaks.  * Some doc.

--- include/geGL/BufferObject.h
+++ include/geGL/BufferObject.h
@@ -4,16 +4,50 @@
 #include <geGL/Standard/OpenGL.h>
 #include <geCore/Object.h>
 
+#include <list>
+
 namespace ge
 {
    namespace gl
 {
+/**
+ * Abstraction of the OpenGL Buffer Object.
+ * Uses Direct State Access. It's internal data pointer can be used either for
+ * mapping or as client-side data storage (not both).
+ */
 class GE_EXPORT BufferObject// : public ge::core::Object
 {
 public:
    BufferObject();
-   BufferObject(const BufferObject& buf, const ge::core::CopyOp& copyop = ge::core::CopyOp::SHALLOW_COPY);
+   BufferObject(const BufferObject& buf/*, const ge::core::CopyOp& copyop = ge::core::CopyOp::SHALLOW_COPY*/);
    virtual ~BufferObject();
+
+   //class GE_EXPORT Mapping
+   //{
+   //public:
+   //   Mapping(GLintptr offset, GLsizeiptr size, GLbitfield flags, GLuint bufferObject, void* data_)
+   //      : _size(size)
+   //      , _flags(flags)
+   //      , _offset(offset)
+   //      , _bufferObject(bufferObject)
+   //      , data(data_)
+   //      //, _myPlace()
+   //   {
+
+   //   }
+
+   //   void *data;
+
+   //   template <typename T> T* getData(){ return static_cast<T*> (_data); }
+   //   //virtual void setPlace(const std::list<Mapping>::iterator& place){ _myPlace = place; }
+   //protected:
+   //   GLsizeiptr _size;
+   //   GLbitfield _flags;
+   //   GLintptr _offset;
+   //   GLuint _bufferObject;
+   //   //std::list<Mapping>::iterator _myPlace;
+
+   //};
 
    //META_GE_Object(BufferObject)
 
@@ -22,20 +56,23 @@ public:
    virtual void createAndInitGLBO(GLenum target, GLenum usage = GL_STATIC_DRAW, bool mutable_ = true, GLbitfield flags = 0);
    virtual void createAndInitGLBO(GLenum target, GLsizeiptr size, GLenum usage = GL_STATIC_DRAW, void* data = NULL, bool mutable_ = true, GLbitfield flags = 0);
    virtual void deleteGLObject();
-   virtual void map(GLenum mapFlags = 0);
+   virtual void map(GLintptr offset = 0, GLsizeiptr size = 0, GLenum mapFlags = 0);
    virtual void unmap();
+   //virtual const Mapping *createMapping(GLintptr offset, GLsizeiptr size, GLbitfield flags);
+   //virtual void destroyMapping(Mapping &mapping);
    virtual GLboolean isMapped();
    virtual void bind();
    virtual void unbind();
 
-   void* data(){ return _data; }
-   GLsizeiptr size(){return _size;}
-   GLuint glName(){ return _bufferObject; }
+   inline void* data(){ return _data; }
+   inline GLsizeiptr size(){return _size;}
+   inline GLuint glName(){ return _bufferObject; }
 
 protected:
 
    bool _mutable;
    void* _data; ///< Designed to be used with this buffer object only. If you use this pointer for e.g. mapping it to another BO you could crash the app.
+   //std::list<Mapping> _mappings;
 
    GLuint _bufferObject; ///< Buffer Object OpenGL ID
    GLenum _target;
--- src/geGL/BufferObject.cpp
+++ src/geGL/BufferObject.cpp
@@ -15,7 +15,10 @@ ge::gl::BufferObject::BufferObject()
 
 }
 
-ge::gl::BufferObject::BufferObject( const BufferObject& buf, const ge::core::CopyOp& copyop /*= ge::core::CopyOp::SHALLOW_COPY*/ )
+/**
+ * Copy constructor, doing only a shallow copy of the data.
+ */
+ge::gl::BufferObject::BufferObject( const BufferObject& buf/*, const ge::core::CopyOp& copyop /*= ge::core::CopyOp::SHALLOW_COPY*/ )
    //: Object(buf, copyop)
    : _mutable(buf._mutable)
    , _data(buf._data)
@@ -35,6 +38,10 @@ ge::gl::BufferObject::~BufferObject()
    if(_data) delete _data;
 }
 
+/**
+ * Allocates client-side buffer. Don't call this method if 
+ * the buffer is mapped, because the same pointer is used.
+ */
 void ge::gl::BufferObject::allocateClientSideBuffer( GLsizeiptr size, void* dataSource/* = NULL*/ )
 {
    _data = malloc(size);
@@ -50,12 +57,21 @@ void ge::gl::BufferObject::allocateClientSideBuffer( GLsizeiptr size, void* data
    _size = size;
 }
 
+/**
+ * Frees client-side buffer. Don't use if the buffer is mapped.
+ */
 void ge::gl::BufferObject::freeClientSideBuffer()
 {
    //not checking if gl is mapped (waste of time)
    if(_data) free(_data);
 }
 
+/**
+ * Inits the OpenGL Buffer Object and uses internal client-side buffer to initialize it.
+ * The allocateClientSideBuffer() should be called before.
+ *
+ * @param mutable_ Controls if the OGL BO is mutable (BufferData) or immutable (BufferStorage).
+ */
 void ge::gl::BufferObject::createAndInitGLBO( GLenum target, GLenum usage /*= GL_STATIC_DRAW*/, bool mutable_ /*= true*/, GLbitfield flags /*= 0*/ )
 {
    if((!_mutable && _bufferObject) || (_size <= 0) )
@@ -86,6 +102,12 @@ void ge::gl::BufferObject::createAndInitGLBO( GLenum target, GLenum usage /*= GL
    }
 }
 
+/**
+ * Inits the OpenGL Buffer Object with supplied data. This does NOT allocate the internal
+ * client-side buffer (_data). This method can be used for preparing the buffer for mapping.
+ *
+ * @param mutable_ Controls if the OGL BO is mutable (BufferData) or immutable (BufferStorage).
+ */
 void ge::gl::BufferObject::createAndInitGLBO( GLenum target, GLsizeiptr size, GLenum usage /*= GL_STATIC_DRAW*/, void* data/* = NULL*/, bool mutable_ /*= true*/, GLbitfield flags/* = 0*/ )
 {
    if(!_mutable && _bufferObject)
@@ -117,25 +139,38 @@ void ge::gl::BufferObject::createAndInitGLBO( GLenum target, GLsizeiptr size, GL
    }
 }
 
+/**
+ * Calls the glDeleteBuffer and sets _bufferObject member to 0.
+ */
 void ge::gl::BufferObject::deleteGLObject()
 {
    glDeleteBuffers(1, &_bufferObject);
    _bufferObject = 0;
 }
 
-void ge::gl::BufferObject::map( GLenum mapFlags/* = 0*/)
+/**
+ * Maps the buffer to it's internal _data member. If client-side buffer has been allocated, the
+ * user is responsible for deallocation prior to calling this function.
+ */
+void ge::gl::BufferObject::map( GLintptr offset /*= 0*/, GLsizeiptr size /*= 0*/, GLenum mapFlags /*= 0*/ )
 {
    //if(_data)
       //should be NULL
-   _data = glMapNamedBufferRangeEXT(_bufferObject, 0, _size, mapFlags==0 ? _flags : mapFlags);
+   _data = glMapNamedBufferRangeEXT(_bufferObject, offset, size==0 ? _size : size, mapFlags==0 ? _flags : mapFlags);
 }
 
+/**
+ * Unmaps the buffer and sets _data to NULL.
+ */
 void ge::gl::BufferObject::unmap()
 {
    glUnmapNamedBufferEXT(_bufferObject);
    _data = NULL;
 }
 
+/**
+ * Asks OGL if the buffer is mapped.
+ */
 GLboolean ge::gl::BufferObject::isMapped()
 {
    GLboolean ret;
@@ -143,12 +178,38 @@ GLboolean ge::gl::BufferObject::isMapped()
    return ret;
 }
 
+/**
+ * Binds the buffer.
+ */
 void ge::gl::BufferObject::bind()
 {
    glBindBuffer(_target, _bufferObject);
 }
 
+/**
+ * Unbinds the target. Note that this calls glBindBuffer(_target, 0) so it will effectively
+ * unbind any buffer bind to the _target. _target is a member initialized in constructor.
+ */
 void ge::gl::BufferObject::unbind()
 {
    glBindBuffer(_target, 0);
 }
+
+//const ge::gl::BufferObject::Mapping* ge::gl::BufferObject::createMapping( GLintptr offset, GLsizeiptr size, GLbitfield flags )
+//{
+//   void *data = glMapNamedBufferRangeEXT(_bufferObject, offset, size, flags);
+//
+//   if(data)
+//   {   
+//      ge::gl::BufferObject::Mapping m(offset, size, flags, _bufferObject, data);
+//      _mappings.emplace_back(std::move(m));
+//      _mappings.back().setPlace(--_mappings.end());
+//      return &(_mappings.back());
+//   }
+//   return NULL;
+//}
+//
+//void ge::gl::BufferObject::destroyMapping( Mapping &mapping )
+//{
+//
+//}
