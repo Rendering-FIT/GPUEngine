88e19704d1f75dff61ed71386073d30713c5805e
dormon <dormon@papovetrak.(none)>
2014-09-01 17:27:39 +0200

    Added If and While command

--- include/geGL/Standard/OpenGL.h
+++ include/geGL/Standard/OpenGL.h
@@ -7,52 +7,72 @@
 
 namespace ge
 {
-   namespace gl
-   {
+  namespace gl
+  {
 
-      class GE_EXPORT Command
-      {
-         public:
-            virtual void apply()=0;
-      };
-
-
-      class GE_EXPORT CommandContainer: public Command
-      {
-         public:
-            Command     * command;
-            bool          ref;
-            void reset();
-            void free ();
-            void apply();
-      };
+    class GE_EXPORT Command
+    {
+      public:
+        virtual void apply()=0;
+    };
 
 
-      class GE_EXPORT CommandList: public Command
-      {
-         public:
-           /**
-            * @brief constructs command list
-            *
-            * @param outOfOrder if it is set to true, commands can be executed out of order
-            */
-            CommandList(bool outOfOrder);
-            void apply();
+    class GE_EXPORT CommandContainer: public Command
+    {
+      public:
+        Command     * command;
+        bool          ref;
+        void reset();
+        void free ();
+        void apply();
+    };
 
-            /*virtual unsigned addCommand(Command* command);
-              virtual unsigned insertCommand(Command* command, unsigned after);
-              virtual void clear();
-              virtual void erase();*/
 
-            bool outOfOrder; ///<
-            std::vector<Command*> commands;
+    class GE_EXPORT CommandList: public Command
+    {
+      public:
+        /**
+         * @brief constructs command list
+         *
+         * @param outOfOrder if it is set to true, commands can be executed out of order
+         */
+        CommandList(bool outOfOrder);
+        void apply();
 
-            /*std::vector<Command*>& getCommands() { return _commands; }
-              bool getAlterable() const { return _alterable; }
-              void setAlterable(bool val) { _alterable = val; }*/
-         protected:
-      };
-   }
+        /*virtual unsigned addCommand(Command* command);
+          virtual unsigned insertCommand(Command* command, unsigned after);
+          virtual void clear();
+          virtual void erase();*/
+
+        bool outOfOrder; ///<
+        std::vector<Command*> commands;
+
+        /*std::vector<Command*>& getCommands() { return _commands; }
+          bool getAlterable() const { return _alterable; }
+          void setAlterable(bool val) { _alterable = val; }*/
+      protected:
+    };
+    class GE_EXPORT CommandStatement: public Command
+    {
+      public:
+        bool     isTrue;
+    };
+    class GE_EXPORT CommandIf: public Command
+    {
+      public:
+        CommandStatement *statement;
+        Command          *trueBranch;
+        Command          *falseBranch;
+        void              apply();
+    };
+    class GE_EXPORT CommandWhile: public Command
+    {
+      public:
+        CommandStatement *statement;
+        Command          *body;
+        void              apply();
+    };
+  }
 }
 
 #endif//_COPENGL_HPP_
--- src/geGL/Standard/OpenGL.cpp
+++ src/geGL/Standard/OpenGL.cpp
@@ -26,6 +26,22 @@ namespace ge
       for(unsigned i=0;i<this->commands.size();++i)
         this->commands[i]->apply();
     }
-
+    void CommandIf::apply(){
+      this->statement->apply();
+      if(this->statement->isTrue){
+        if(this->trueBranch)this->trueBranch->apply();
+      }else{
+        if(this->falseBranch)this->falseBranch->apply();
+      }
+    }
+    //stepable while, semistepable while, deepstepable command
+    void CommandWhile::apply(){
+      if(!this->body)return;
+      for(;;){
+        this->statement->apply();
+        if(!this->statement->isTrue)break;
+        this->body->apply();
+      }
+    }
   }//ogl
 }//ge
