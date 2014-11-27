9aafade248777369efa6a0163fda1783ab763e01
dormon <dormon@papovetrak.(none)>
2014-11-27 13:41:55 +0100

    FIX define GLM_FORCE_RADIANS inside CameraObject

--- include/geUtil/CameraObject.h
+++ include/geUtil/CameraObject.h
@@ -2,6 +2,7 @@
 #define _CAMERAOBJECT_H_
 
 #include<glm/glm.hpp>
+#define GLM_FORCE_RADIANS
 #include<glm/gtc/matrix_transform.hpp>
 #include<glm/gtc/type_ptr.hpp>
 #include<glm/gtc/matrix_access.hpp>
