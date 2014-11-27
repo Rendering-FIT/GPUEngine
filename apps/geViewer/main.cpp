57806ae15afe0902f53b6c6930a945a7e200f90b
dormon <imilet@fit.vutbr.cz>
2014-11-27 15:38:06 +0100

    Added FindX11.cmake to CMakeModules and CMakeList.txt Added X11_LIBRARIES to EmptyWindow/CMakeLists.txt and geViewer/CMakeLists.txt Added glClear(COLOR|DEPTH) to geViewer/main.cpp

--- apps/geViewer/main.cpp
+++ apps/geViewer/main.cpp
@@ -101,6 +101,7 @@ static AttribsConfig ac;
 
 
 void Idle(){
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
    glProgram->use();
    attribsRef.attribsStorage->bind();
    //glDrawArrays(GL_TRIANGLES,0,6);
