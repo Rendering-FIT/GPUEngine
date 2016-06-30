824181eea5a3601d80b13b999848123fe7fbeacf
dormon <imilet@fit.vutbr.cz>
2016-06-30 20:09:58 +0200

    Changed GPUENGINE_DYNAMIC to GPUENGINE_DYNAMIC_LINKAGE in geCore geDe geGL geParser geRG geSG geAd/OsgImport geAd/SDLWindow and geUtil CMakeLists.txt

--- include/geCore/Command.h
+++ include/geCore/Command.h
@@ -1,115 +1,30 @@
 #pragma once
 
-#include <geCore/Export.h>
-#include <geCore/Functor.h>
-#include<iostream>
+#include<geCore/Export.h>
+#include<geCore/Functor.h>
 #include<vector>
-#include<map>
 #include<memory>
 
 namespace ge{
   namespace core{
     class GECORE_EXPORT Command : public Functor{
-      protected:
-        virtual void*_getAttribAddress(std::string){return NULL;}
       public:
         virtual void operator()(){};
         virtual ~Command(){};
-        template<typename T>T&getAttrib(std::string name){
-          return *((T*)this->_getAttribAddress(name));
-        }
     };
-
-    class GECORE_EXPORT CommandContainer: public Command
-    {
-      protected:
-        Command**_command;
-      public:
-        CommandContainer(Command**command=NULL);
-        virtual ~CommandContainer();
-        virtual void operator()();
-        void set(Command**command=NULL);
-        Command**get();
-        Command* getCommand();
-    };
-
 
     class GECORE_EXPORT CommandList: public std::vector<Command*>, public Command{
       public:
         CommandList();
         virtual ~CommandList();
-        virtual void operator()();
+        virtual void operator()()override;
     };
 
     class GECORE_EXPORT SharedCommandList: public std::vector<std::shared_ptr<Command>>, public Command{
       public:
         SharedCommandList();
         virtual ~SharedCommandList();
-        virtual void operator()();
-    };
-
-    /*
-    class GECORE_EXPORT CommandList: public Command
-    {
-      protected:
-        bool _outOfOrder;
-        bool _commutative;
-        bool _associative;
-        std::vector<Command*> _commands;
-        unsigned _commandToExecute;///<index of command that will be executed using step()
-      public:
-        CommandList(bool outOfOrder=false);
-        virtual ~CommandList();
-        virtual unsigned add(Command*command);
-        Command* getCommand(unsigned i);
-        unsigned size();
-        virtual void operator()();
-        void step();
-    };
-
-    class GECORE_EXPORT SharedCommandList: public Command{
-      protected:
-        std::vector<std::shared_ptr<Command>>_commands;
-      public:
-        SharedCommandList();
-        virtual ~SharedCommandList();
-        std::shared_ptr<Command>&getCommand(unsigned i);
-        unsigned size();
-        virtual void operator()();
-    };*/
-
-    class GECORE_EXPORT CommandListWithAccessor: public CommandList{
-      protected:
-        std::map<std::string,Command*>_name2Command;
-      public:
-        CommandListWithAccessor();
-        virtual Command*operator[](std::string name);
-        virtual unsigned add(Command*command,std::string name="");
-    };
-
-    class GECORE_EXPORT CommandStatement: public Command
-    {
-      public:
-        bool     isTrue;
-    };
-    class GECORE_EXPORT CommandIf: public Command
-    {
-      public:
-        CommandStatement *statement;
-        Command* trueBranch;
-        Command* falseBranch;
-        virtual void operator()();
-    };
-    class GECORE_EXPORT CommandWhile: public Command
-    {
-      public:
-        CommandStatement* statement;
-        Command*          body;
-        virtual void operator()();
-    };
-    class GECORE_EXPORT CommandInterpret
-    {
-
+        virtual void operator()()override;
     };
 
   }
--- src/geCore/Command.cpp
+++ src/geCore/Command.cpp
@@ -3,49 +3,6 @@
 
 using namespace ge::core;
 
-CommandContainer::CommandContainer(Command**command){
-  this->_command = command;
-}
-CommandContainer::~CommandContainer(){
-  this->_command = NULL;
-}
-void CommandContainer::operator()(){
-  if(this->_command)(*(*this->_command))();
-}
-void CommandContainer::set(Command**command){
-  this->_command = command;
-}
-Command**CommandContainer::get(){
-  return this->_command;
-}
-Command*CommandContainer::getCommand(){
-  if(this->_command)return *this->_command;
-  return NULL;
-}
-
-/*
-void CommandList::operator()(){
-  for(unsigned i=0;i<this->_commands.size();++i)
-    (*this->_commands[i])();
-}
-
-CommandList::CommandList(bool outOfOrder){
-  this->_outOfOrder=outOfOrder;
-}
-CommandList::~CommandList(){
-  for(unsigned i=0;i<this->_commands.size();++i)
-    ge::core::deleteSetNull(this->_commands[i]);
-}
-unsigned CommandList::add(Command*commands){
-  this->_commands.push_back(commands);
-  return this->_commands.size()-1;
-}
-Command* CommandList::getCommand(unsigned i){
-  if(i>=this->_commands.size())return NULL;
-  return this->_commands[i];
-}
-unsigned CommandList::size(){return this->_commands.size();}
-*/
 CommandList::CommandList(){}
 CommandList::~CommandList(){
   for(auto x:*this)delete x;
@@ -58,37 +15,5 @@ SharedCommandList::SharedCommandList(){}
 SharedCommandList::~SharedCommandList(){}
 void SharedCommandList::operator()(){
   for(auto x:*this)(*x)();
-}
-
-CommandListWithAccessor::CommandListWithAccessor():CommandList(){}
-
-Command*CommandListWithAccessor::operator[](std::string name){
-  auto it=this->_name2Command.find(name);
-  if(it==this->_name2Command.end())return NULL;
-  return this->_name2Command[name];
-}
-
-unsigned CommandListWithAccessor::add(Command*command,std::string name){
-  if(name!="")this->_name2Command[name]=command;
-  this->CommandList::push_back(command);
-  return (unsigned)this->size()-1;
-}
-
-void CommandIf::operator()(){
-  (*this->statement)();
-  if(this->statement->isTrue){
-    if(this->trueBranch)(*this->trueBranch)();
-  }else{
-    if(this->falseBranch)(*this->falseBranch)();
-  }
-}
-//stepable while, semistepable while, deepstepable command
-void CommandWhile::operator()(){
-  if(!this->body)return;
-  for(;;){
-    (*this->statement)();
-    if(!this->statement->isTrue)break;
-    (*this->body)();
-  }
 }
 
