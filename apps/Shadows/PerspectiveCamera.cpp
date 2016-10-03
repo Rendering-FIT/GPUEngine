b49e3ba3c89dbe9912c54c31130d97fe1814f3e2
dormon <imilet@fit.vutbr.cz>
2016-10-03 20:01:24 +0200

    Added infinite perpective camera

--- apps/Shadows/PerspectiveCamera.cpp
+++ apps/Shadows/PerspectiveCamera.cpp
@@ -33,6 +33,33 @@ void PerspectiveCamera::setFovy  (float value){this->_fovy   = value;this->_reco
 void PerspectiveCamera::setAspect(float value){this->_aspect = value;this->_recompute = true;}
 
 void PerspectiveCamera::_compute(){
-  this->_projection = glm::perspective(this->_fovy,this->_aspect,this->_near,this->_far);
+  if(this->_far != std::numeric_limits<float>::infinity()){
+    this->_projection = glm::perspective(this->_fovy,this->_aspect,this->_near,this->_far);
+    return;
+  }
+  float top=glm::tan(this->_fovy/2)*this->_near;//top border of viewport
+  float bottom=-top;//bottom border of viewport
+  float right=top*this->_aspect;//right border of viewport
+  float left=-right;//left border of viewport
+  //0. column
+  this->_projection[0][0]=2*this->_near/(right-left);
+  this->_projection[0][1]=0;
+  this->_projection[0][2]=0;
+  this->_projection[0][3]=0;
+  //1. column
+  this->_projection[1][0]=0;
+  this->_projection[1][1]=2*this->_near/(top-bottom);
+  this->_projection[1][2]=0;
+  this->_projection[1][3]=0;
+  //2. column
+  this->_projection[2][0]=(right+left)/(right-left);
+  this->_projection[2][1]=(top+bottom)/(top-bottom);
+  this->_projection[2][2]=-1;
+  this->_projection[2][3]=-1;
+  //3. column
+  this->_projection[3][0]=0;
+  this->_projection[3][1]=0;
+  this->_projection[3][2]=-2*this->_near;
+  this->_projection[3][3]=0;
 }
 
