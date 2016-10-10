8a82d38897ca76f62301104044b5a4e9bc114d9c
PCJohn <peciva@fit.vutbr.cz>
2016-10-10 16:31:20 +0200

    Improved geRG::AttribConfig::updateId() to better encode attrib configuration.

--- src/geRG/AttribConfig.cpp
+++ src/geRG/AttribConfig.cpp
@@ -48,93 +48,68 @@ void AttribConfig::Instance::deleteAllAttribStorages()
 }
 
 
+static AttribConfigId getAttribTypeId(const AttribType t)
+{
+   if(t==AttribType::Empty)
+      return 0;
+   else
+      switch(t.glType()) {
+         case GLType::FLOAT:
+            switch(t.numComponents()) {
+               case 2: return 1;
+               case 3: return 2;
+               case 4: return 3;
+               default: return unsigned(-1);
+            }
+         case GLType::HALF_FLOAT:
+            switch(t.numComponents()) {
+               case 2: return 4;
+               case 3: return 5;
+               case 4: return 6;
+               default: return unsigned(-1);
+            }
+         case GLType::UNSIGNED_BYTE:
+            switch(t.numComponents()) {
+               case 2: return 7;
+               case 3: return 8;
+               case 4: return 9;
+               default: return unsigned(-1);
+            }
+         default: return unsigned(-1);
+      }
+}
+
+
 AttribConfigId AttribConfig::getId(const std::vector<AttribType>& attribTypes,bool ebo)
 {
    unsigned configSize=unsigned(attribTypes.size());
-   if(configSize>4||configSize==0)
+
+   // remove ending AttribType::Empty's
+   while(configSize>=1&&attribTypes[configSize-1]==AttribType::Empty)
+      configSize--;
+
+   // return 0 for more than 4 attribs or zero attribs
+   if(configSize>7||configSize==0)
       return 0;
 
-   uint16_t r=0;
+   AttribConfigId r=0;
    AttribType t;
 
    // indices
+   // (if they are used, they required EBO to use UNSIGNED_INT type)
    //
-   // Use of indices is signalled by ebo boolean.
-   // If they are used, they required EBO to use UNSIGNED_INT type.
-   //
-   // Indices occupy bit 0 of the returned integer.
-   if(ebo)  r|=0x1;
+   // Indices occupy bit 28 of the returned integer.
+   if(ebo)
+      r|=0x1<<28;
 
-   // coordinates
-   //
-   // Coordinates are expected to be at attribute index 0
-   // and we expect that they are always specified.
-   //
-   // Coordinates occupies bits 1 and 2 of the returned integer.
-   t=attribTypes[0];
-   if(t==AttribType::Vec3)  r|=0x2;
-   else if(t==AttribType::Vec4)  r|=0x4;
-   else if(t==AttribType::Vec2)  r|=0x6;
-   else return 0;
-   if(configSize==1)
-      return r;
-
-   // texCoords
-   //
-   // Texture coordinates are not mandatory.
-   // We expect that they are stored at the last attribute index
-   // (last index in AttribConfig is occupied by indices flag,
-   // so it is just before it).
-   //
-   // TexCoords occupies bits 8 and 9 of the returned integer.
-   unsigned texCoordPossibleIndex=configSize-1;
-   t=attribTypes[texCoordPossibleIndex];
-   if(t==AttribType::Empty);
-   else if(t==AttribType::Vec2)  r|=0x100;
-   else if(t==AttribType::HVec2)  r|=0x200;
-   else texCoordPossibleIndex=0;
-   if(configSize==2&&texCoordPossibleIndex!=0)
-      return r;
-   int numToParse=texCoordPossibleIndex!=0?int(configSize)-2:int(configSize)-1;
-
-   // normals
-   //
-   // Normals are not mandatory.
-   // If we find some 3-component formats,
-   // let's consider them normals, although they may by colors as well.
-   //
-   // Normals occupies bits 3 and 4 of the returned integer.
-   bool normalsFound=true;
-   t=attribTypes[1];
-   if(t==AttribType::Empty);
-   else if(t==AttribType::Vec3)  r|=0x8;
-   else if(t==AttribType::HVec3)  r|=0x10;
-   else if(t==AttribType::UBVec3)  r|=0x18;
-   else normalsFound=false;
-   if(normalsFound) {
-      numToParse--;
-      if(numToParse==0)
-         return r;
+   // generate id out of attrib types
+   // (each attrib occupies 4 bits of returned integer)
+   for(unsigned i=0,s=24; i<configSize; i++,s-=4) {
+      AttribConfigId id=getAttribTypeId(attribTypes[i]);
+      if(id==unsigned(-1))
+         return 0;
+      r|=id<<s;
    }
-
-   // color
-   //
-   // Colors are not mandatory. If they are specified, they have to use attribute index 1 or 2.
-   // Index 1 must be used if there are no normals, index 2 must be used when normals are in use.
-   //
-   // Color occupies bits 5,6 and 7 of the returned integer.
-   t=attribTypes[normalsFound?2:1];
-   if(t==AttribType::Empty);
-   else if(t==AttribType::Vec3)  r|=0x20;
-   else if(t==AttribType::Vec4)  r|=0x40;
-   else if(t==AttribType::HVec3)  r|=0x60;
-   else if(t==AttribType::HVec4)  r|=0x80;
-   else if(t==AttribType::UBVec3)  r|=0xa0;
-   else if(t==AttribType::UBVec4)  r|=0xc0;
-   //else if(t==Array::makeType(ArrayAdapter::GLType::UNSIGNED_INT_8_8_8_8,1)  r|=0x1c0;
-   //else if(t==Array::makeType(ArrayAdapter::GLType::UNSIGNED_INT_8_8_8_8_REV,1)  r|=0x200;
-   //else if(t==Array::makeType(ArrayAdapter::GLType::UNSIGNED_BYTE,ArrayAdapter::BGRA)  r|=0x240;
-   else return 0;
 
    return r;
 }
