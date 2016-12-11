bd4f4799bca143671fb338639936e508945c955b
dormon <imilet@fit.vutbr.cz>
2016-12-11 17:57:50 +0100

    FIXED VSSV sides multiplicity ADDED new multiplicity computation based on planes

--- apps/Shadows/VSSVShaders.h
+++ apps/Shadows/VSSVShaders.h
@@ -2,11 +2,11 @@
 
 #include<iostream>
 const std::string _drawSidesVertexShaderSrc = R".(
-#version 450
-
 #ifndef MAX_MULTIPLICITY
 #define MAX_MULTIPLICITY 2
 #endif//MAX_MULTIPLICITY
+
+#ifndef USE_PLANES_INSTEAD_OF_OPPOSITE_VERTICES
 
 layout(location=0)in vec3  edgeVertexA        ;
 layout(location=1)in vec3  edgeVertexB        ;
@@ -28,6 +28,7 @@ int computeMult(vec3 A,vec3 B,vec3 C,vec4 L){
 }
 
 int currentMultiplicity(vec3 A,vec3 B,vec3 O,vec4 L){
+  return int(sign(dot(cross(B-A,L.xyz-A),O-A)));
   if(greaterVec(A,O)>0)
     return computeMult(O,A,B,L);
   else if(greaterVec(B,O)>0)
@@ -38,10 +39,8 @@ int currentMultiplicity(vec3 A,vec3 B,vec3 O,vec4 L){
 
 void main(){
   mat4 mvp = projectionMatrix*viewMatrix*modelMatrix;
-  //int vertexIDCCW = int(gl_VertexID>2?gl_VertexID-2:2-gl_VertexID);
-  //int vertexIDCW  = int(gl_VertexID>2?6-gl_VertexID:gl_VertexID);
   int vertexIDCCW = gl_VertexID;
-  int vertexIDCW  = 4-gl_VertexID;
+  int vertexIDCW  = gl_VertexID^0x1;
   int sideID      = gl_InstanceID%MAX_MULTIPLICITY;
   vec4 P[4];
   P[0]=vec4(edgeVertexA.xyz,1);
@@ -50,7 +49,7 @@ void main(){
   P[3]=vec4(P[1].xyz*lightPosition.w-lightPosition.xyz,0);
   int multiplicity = 0;
   for(uint m=0;m<uint(nofOppositeVertices) && m<2;++m)
-    multiplicity += currentMultiplicity(P[0].xyz,P[1].xyz,oppositeVertices[m].xyz,lightPosition);
+    multiplicity -= currentMultiplicity(P[0].xyz,P[1].xyz,oppositeVertices[m].xyz,lightPosition);
   if(sideID<int(abs(float(multiplicity)))){
     if(multiplicity>0)
       gl_Position=mvp*P[vertexIDCCW];
@@ -59,6 +58,44 @@ void main(){
   }else
     gl_Position=vec4(0,0,0,0);
 }
+
+#else//USE_PLANES_INSTEAD_OF_OPPOSITE_VERTICES
+#line 62
+layout(location=0)in vec3  edgeVertexA      ;
+layout(location=1)in vec3  edgeVertexB      ;
+layout(location=2)in float nofOppositeVertices;
+layout(location=3)in vec4  trianglePlanes[MAX_MULTIPLICITY];
+
+uniform vec4 lightPosition    = vec4(10,10,10,1);
+uniform mat4 modelMatrix      = mat4(1.f);
+uniform mat4 viewMatrix       = mat4(1.f);
+uniform mat4 projectionMatrix = mat4(1.f);
+
+void main(){
+  mat4 mvp = projectionMatrix*viewMatrix*modelMatrix;
+  int vertexIDCCW = gl_VertexID;
+  int vertexIDCW  = gl_VertexID^0x1;
+  int sideID      = gl_InstanceID%MAX_MULTIPLICITY;
+  vec4 P[4];
+  P[0]=vec4(edgeVertexA.xyz,1);
+  P[1]=vec4(edgeVertexB.xyz,1);
+  P[2]=vec4(P[0].xyz*lightPosition.w-lightPosition.xyz,0);
+  P[3]=vec4(P[1].xyz*lightPosition.w-lightPosition.xyz,0);
+  int multiplicity = 0;
+  for(uint m=0;m<uint(nofOppositeVertices) && m<2;++m)
+    multiplicity += int(sign(dot(trianglePlanes[m],lightPosition)));
+  if(sideID<int(abs(float(multiplicity)))){
+    if(multiplicity>0)
+      gl_Position=mvp*P[vertexIDCCW];
+    if(multiplicity<0)
+      gl_Position=mvp*P[vertexIDCW];
+  }else
+    gl_Position=vec4(0,0,0,0);
+}
+
+
+#endif
+
 ).";
 
 
