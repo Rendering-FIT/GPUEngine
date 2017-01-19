42bb5fe5dc5ef0e20b5cadd0e4a95716e63e221b
dormon <imilet@fit.vutbr.cz>
2017-01-19 20:35:27 +0100

    app/Shadows - refactored VSSV

--- apps/Shadows/VSSV.h
+++ apps/Shadows/VSSV.h
@@ -29,7 +29,7 @@ class VSSV: public ShadowMethod{
     std::shared_ptr<ge::gl::Framebuffer>_maskFbo                = nullptr;
     std::shared_ptr<ge::gl::VertexArray>_maskVao                = nullptr;
 
-    std::shared_ptr<ge::gl::Program>    _drawSides              = nullptr;
+    std::shared_ptr<ge::gl::Program>    _drawSidesProgram       = nullptr;
     std::shared_ptr<ge::gl::Buffer>     _adjacency              = nullptr;
     std::shared_ptr<ge::gl::VertexArray>_sidesVao               = nullptr;
     size_t                              _nofEdges               = 0;
@@ -37,11 +37,11 @@ class VSSV: public ShadowMethod{
     size_t                              _nofTriangles           = 0;
     bool                                _zfail                  = true;
 
-    std::shared_ptr<ge::gl::Program>    _drawCaps               = nullptr;
+    std::shared_ptr<ge::gl::Program>    _drawCapsProgram        = nullptr;
     std::shared_ptr<ge::gl::VertexArray>_capsVao                = nullptr;
     std::shared_ptr<ge::gl::Buffer>     _caps                   = nullptr;
 
-    std::shared_ptr<ge::gl::Program>    _blit                   = nullptr;
+    std::shared_ptr<ge::gl::Program>    _blitProgram            = nullptr;
     std::shared_ptr<ge::gl::VertexArray>_emptyVao               = nullptr;
     std::shared_ptr<TimeStamp>          _timeStamper            = nullptr;
     bool                                _usePlanes              = false;
@@ -51,4 +51,13 @@ class VSSV: public ShadowMethod{
     void                                _createSideDataUsingAllPlanes(std::shared_ptr<Adjacency>const&adj);
     void                                _createSideDataUsingPlanes   (std::shared_ptr<Adjacency>const&adj);
     void                                _createCapDataUsingPoints    (std::shared_ptr<Adjacency>const&adj);
+    void _drawSides(
+        glm::vec4 const&lightPosition,
+        glm::mat4 const&view         ,
+        glm::mat4 const&projection   );
+    void _drawCaps(
+        glm::vec4 const&lightPosition,
+        glm::mat4 const&view         ,
+        glm::mat4 const&projection   );
+    void _blit();
 };
