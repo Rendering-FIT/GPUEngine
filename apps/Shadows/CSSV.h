f9c43e6f305ffcc089399d97286a0e6cff522684
dormon <imilet@fit.vutbr.cz>
2017-01-25 21:47:08 +0100

    apps/Shadows - added interleaving for triangle planes in CSSV method

--- apps/Shadows/CSSV.h
+++ apps/Shadows/CSSV.h
@@ -11,6 +11,7 @@ struct CSSVParams{
   bool   localAtomic     = true ;
   bool   cullSides       = false;
   bool   usePlanes       = false;
+  bool   useInterleaving = false;
 };
 
 class Adjacency;
@@ -49,7 +50,8 @@ class CSSV: public ShadowVolumes{
 
     void _computeSides(glm::vec4 const&lightPosition);
 
-    void _createSidesData           (std::shared_ptr<Adjacency const>const&adj);
-    void _createSidesDataUsingPlanes(std::shared_ptr<Adjacency const>const&adj);
-    void _createCapsData            (std::shared_ptr<Adjacency const>const&adj);
+    void _createSidesData                           (std::shared_ptr<Adjacency const>const&adj);
+    void _createSidesDataUsingPlanes                (std::shared_ptr<Adjacency const>const&adj);
+    void _createSidesDataUsingPlanesWithInterleaving(std::shared_ptr<Adjacency const>const&adj);
+    void _createCapsData                            (std::shared_ptr<Adjacency const>const&adj);
 };
--- apps/Shadows/CSSVShaders.h
+++ apps/Shadows/CSSVShaders.h
@@ -52,100 +52,125 @@ void main(){
 
   uint gid=gl_GlobalInvocationID.x;
 
-  if(gid<numEdge){//compute silhouette
+  if(gid>=numEdge)return;
 #if CULL_SIDES == 1
-    vec4 P[4];
+  vec4 P[4];
 #else
-    vec4 P[2];
+  vec4 P[2];
 #endif
 #if     USE_PLANES == 1
-    gid*=3+3+4*MAX_MULTIPLICITY;
+  gid*=3+3+4*MAX_MULTIPLICITY;
 #else //USE_PLANES == 1
-    gid*=2+MAX_MULTIPLICITY;
+  gid*=2+MAX_MULTIPLICITY;
 #endif//USE_PLANES == 1
 
 #if     USE_PLANES == 1
-    P[0] = vec4(edges[gid+0],edges[gid+1],edges[gid+2],1);
-    P[1] = vec4(edges[gid+3],edges[gid+4],edges[gid+5],1);
+
+#if     USE_INTERLEAVING == 1
+  P[0].x = edges[gl_GlobalInvocationID.x+numEdge*0];
+  P[0].y = edges[gl_GlobalInvocationID.x+numEdge*1];
+  P[0].z = edges[gl_GlobalInvocationID.x+numEdge*2];
+  P[0].w = 1;
+  P[1].x = edges[gl_GlobalInvocationID.x+numEdge*3];
+  P[1].y = edges[gl_GlobalInvocationID.x+numEdge*4];
+  P[1].z = edges[gl_GlobalInvocationID.x+numEdge*5];
+  P[1].w = 1;
+#else //USE_INTERLEAVING == 1
+  P[0] = vec4(edges[gid+0],edges[gid+1],edges[gid+2],1);
+  P[1] = vec4(edges[gid+3],edges[gid+4],edges[gid+5],1);
+#endif//USE_INTERLEAVING == 1
+
 #else //USE_PLANES == 1
-    P[0]=edges[gid+0];
-    P[1]=edges[gid+1];
+  P[0]=edges[gid+0];
+  P[1]=edges[gid+1];
 #endif//USE_PLANES == 1
 
 #if CULL_SIDES == 1
-    P[2]=vec4(P[0].xyz*lightPosition.w-lightPosition.xyz,0);
-    P[3]=vec4(P[1].xyz*lightPosition.w-lightPosition.xyz,0);
+  P[2]=vec4(P[0].xyz*lightPosition.w-lightPosition.xyz,0);
+  P[3]=vec4(P[1].xyz*lightPosition.w-lightPosition.xyz,0);
 #endif
 
-    int Num=int(P[0].w)+2;
-    P[0].w=1;
+  int Num=int(P[0].w)+2;
+  P[0].w=1;
 
 #if CULL_SIDES == 1
-    vec4 ClipP[4];
-    for(int i=0;i<4;++i)
-      ClipP[i]=mvp*P[i];
+  vec4 ClipP[4];
+  for(int i=0;i<4;++i)
+    ClipP[i]=mvp*P[i];
 
-    vec3 Normal=(mvp*vec4(cross(
-            P[1].xyz-P[0].xyz,
-            lightPosition.xyz-P[0].xyz*lightPosition.w),0)).xyz;
-    ivec3 Corner=ivec3(1+sign(Normal))>>1;
-    if(Corner.z==1);Corner=ivec3(1)-Corner;
-    int Diag=Corner.x+(Corner.y<<1)-1;
+  vec3 Normal=(mvp*vec4(cross(
+          P[1].xyz-P[0].xyz,
+          lightPosition.xyz-P[0].xyz*lightPosition.w),0)).xyz;
+  ivec3 Corner=ivec3(1+sign(Normal))>>1;
+  if(Corner.z==1);Corner=ivec3(1)-Corner;
+  int Diag=Corner.x+(Corner.y<<1)-1;
 
-    if(!isVisible(ClipP,Diag))return;
+  if(!isVisible(ClipP,Diag))return;
 #endif//CULL_SIDES
 
-    precise int Multiplicity=0;
-    if(Num>20)Num=0;
-    if(Num<0)Num=0;
+  precise int Multiplicity=0;
+  if(Num>20)Num=0;
+  if(Num<0)Num=0;
 
 #if     USE_PLANES == 1
-    for(uint m=0;m<MAX_MULTIPLICITY;++m)
-      Multiplicity += int(sign(dot(vec4(edges[gid+6+m*4+0],edges[gid+6+m*4+1],edges[gid+6+m*4+2],edges[gid+6+m*4+3]),lightPosition)));
+
+#if     USE_INTERLEAVING == 1
+  for(uint m=0;m<MAX_MULTIPLICITY;++m){
+    vec4 plane;
+    plane.x = edges[gl_GlobalInvocationID.x+numEdge*(6+m*4+0)];
+    plane.y = edges[gl_GlobalInvocationID.x+numEdge*(6+m*4+1)];
+    plane.z = edges[gl_GlobalInvocationID.x+numEdge*(6+m*4+2)];
+    plane.w = edges[gl_GlobalInvocationID.x+numEdge*(6+m*4+3)];
+    Multiplicity += int(sign(dot(plane,lightPosition)));
+  }
+#else //USE_INTERLEAVING == 1
+  for(uint m=0;m<MAX_MULTIPLICITY;++m)
+    Multiplicity += int(sign(dot(vec4(edges[gid+6+m*4+0],edges[gid+6+m*4+1],edges[gid+6+m*4+2],edges[gid+6+m*4+3]),lightPosition)));
+#endif//USE_INTERLEAVING == 1
+
 #else //USE_PLANES == 1
-    for(int i=2;i<Num;++i)
-      Multiplicity += currentMultiplicity(P[0].xyz,P[1].xyz,edges[gid+i].xyz,lightPosition);
+  for(int i=2;i<Num;++i)
+    Multiplicity += currentMultiplicity(P[0].xyz,P[1].xyz,edges[gid+i].xyz,lightPosition);
 #endif//USE_PLANES == 1
 
 #if LOCAL_ATOMIC == 1
-    uint localOffset = atomicAdd(localCounter,uint(2*abs(Multiplicity)));
-    barrier();
-    if(gl_LocalInvocationID.x==0){
-      globalOffset = atomicAdd(Counter[0],localCounter);
-    }
-    barrier();
-    uint WH = globalOffset + localOffset;
-    if(Multiplicity>0){
-      for(int m=0;m<Multiplicity;++m){
-        silhouettes[WH++]=P[1];
-        silhouettes[WH++]=P[0];
-      }
-    }
-    if(Multiplicity<0){
-      Multiplicity=-Multiplicity;
-      for(int m=0;m<Multiplicity;++m){
-        silhouettes[WH++]=P[0];
-        silhouettes[WH++]=P[1];
-      }
-    }
-#else
-    if(Multiplicity>0){
-      uint WH=atomicAdd(Counter[0],2*Multiplicity);
-      for(int m=0;m<Multiplicity;++m){
-        silhouettes[WH++]=P[1];
-        silhouettes[WH++]=P[0];
-      }
-    }
-    if(Multiplicity<0){
-      Multiplicity=-Multiplicity;
-      uint WH=atomicAdd(Counter[0],2*Multiplicity);
-      for(int m=0;m<Multiplicity;++m){
-        silhouettes[WH++]=P[0];
-        silhouettes[WH++]=P[1];
-      }
-    }
-#endif
+  uint localOffset = atomicAdd(localCounter,uint(2*abs(Multiplicity)));
+  barrier();
+  if(gl_LocalInvocationID.x==0){
+    globalOffset = atomicAdd(Counter[0],localCounter);
   }
+  barrier();
+  uint WH = globalOffset + localOffset;
+  if(Multiplicity>0){
+    for(int m=0;m<Multiplicity;++m){
+      silhouettes[WH++]=P[1];
+      silhouettes[WH++]=P[0];
+    }
+  }
+  if(Multiplicity<0){
+    Multiplicity=-Multiplicity;
+    for(int m=0;m<Multiplicity;++m){
+      silhouettes[WH++]=P[0];
+      silhouettes[WH++]=P[1];
+    }
+  }
+#else
+  if(Multiplicity>0){
+    uint WH=atomicAdd(Counter[0],2*Multiplicity);
+    for(int m=0;m<Multiplicity;++m){
+      silhouettes[WH++]=P[1];
+      silhouettes[WH++]=P[0];
+    }
+  }
+  if(Multiplicity<0){
+    Multiplicity=-Multiplicity;
+    uint WH=atomicAdd(Counter[0],2*Multiplicity);
+    for(int m=0;m<Multiplicity;++m){
+      silhouettes[WH++]=P[0];
+      silhouettes[WH++]=P[1];
+    }
+  }
+#endif
 }).";
 
 
