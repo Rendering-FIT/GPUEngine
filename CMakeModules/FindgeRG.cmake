e2eeba354eb314fe3814c4dc747997c61e4032fa
foreigner <starka@fit.vutbr.cz>
2018-05-15 10:19:28 +0200

    geRG deletion And some minor fixes

--- CMakeModules/FindgeRG.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Internal Find for GPUE module
-#
-
-if(TARGET ${CMAKE_FIND_PACKAGE_NAME})
-   set(${${CMAKE_FIND_PACKAGE_NAME}_FOUND} true)
-else()
-   message("Cannot find ${CMAKE_FIND_PACKAGE_NAME} package. Configure Cmake to build it.")
-endif()
--- CMakeModules/FindgeUtil.cmake
+++ CMakeModules/FindgeUtil.cmake
@@ -5,5 +5,5 @@
 if(TARGET ${CMAKE_FIND_PACKAGE_NAME})
    set(${${CMAKE_FIND_PACKAGE_NAME}_FOUND} true)
 else()
-   message("Cannot find ${CMAKE_FIND_PACKAGE_NAME} package. Configure Cmake to build it.")
+   message("Cannot find ${CMAKE_FIND_PACKAGE_NAME} package. Configure Cmake to build it by checking the GPUENGINE_BUILD_GEUTIL option.")
 endif()
